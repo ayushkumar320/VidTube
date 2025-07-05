35f281422de9545903b5a7bc4e8d25f8a5ed94de
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-05 22:59:07 +0530

    add multer and cloudinary to codebase

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 // Import Routes
 
@@ -22,6 +24,6 @@ import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 // Routes
 
-app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export {app};
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,24 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded on cloudinary. File src: " + response.url);
+    // Once the file is uploaded we would like to delete it from the server
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
