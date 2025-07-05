379ab9dbad8d370516d7bc1958989f63e5880b86
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-05 21:39:45 +0530

    Add hooks to user model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -13,7 +13,8 @@
 */
 
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 // if not destructured i.e {Schema} above.
 // const userSchema = new mongoose.Schema()
 
@@ -64,5 +65,41 @@ const userSchema = new Schema(
   },
   {timestamps: true}
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  // short lived access token
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  // long lived refresh token
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
