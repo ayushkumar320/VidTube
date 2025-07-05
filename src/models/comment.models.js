54ea06f91df06ae24517af33945d749bfbb08581
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-05 14:41:32 +0530

    Add comment model to codebase

---
+++ src/models/comment.models.js
@@ -0,0 +1,27 @@
+/*
+  video ObjectId videos
+  owner ObjectId users
+  content string
+  createdAt Date
+  updatedAt Date
+
+*/
+
+import mongoose, {Schema} from "mongoose";
+const commentSchema = new Schema(
+  {
+    vide: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  {timestamps: true}
+);
