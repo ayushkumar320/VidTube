a9b8707a614074d613b46add054e5c270bee5d89
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-05 13:48:01 +0530

    add user model schema to codebase

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -19,8 +19,50 @@ import mongoose, {Schema} from "mongoose";
 
 const userSchema = new Schema(
   {
-    username: String,
-  }
-)
+    // username: String,
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    emial: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // coudinary URL
+      required: true,
+    },
+    coverImage: {
+      type: String, // coudinary URL
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required!"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {timestamps: true}
+);
 
-export const User = mongoose.model("User", userSchema)
+export const User = mongoose.model("User", userSchema);
