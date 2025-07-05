0ce198fc1719e3b88fde1edb76d5a3ab4c782581
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-05 10:33:51 +0530

    add database connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 import {app} from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./src/.env",
 });
+
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`);
-});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}...`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error!!!");
+  });
