10682add5a1ee5f208f9e1ad0a0d534823584cbd
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-05 15:07:58 +0530

    add plugin to comment schema

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -8,6 +8,7 @@
 */
 
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const commentSchema = new Schema(
   {
     vide: {
@@ -25,4 +26,5 @@ const commentSchema = new Schema(
   },
   {timestamps: true}
 );
+commentSchema.plugin(mongooseAggregatePaginate);
 export const Comment = mongoose.model("Comment", commentSchema);
