b5a86ddf3220ab2c0b0c4c553d31e5129afc05ff
ayushkumar320 <75619981+ayushkumar320@users.noreply.github.com>
2025-07-07 00:18:19 +0530

    Add middleware in authenication

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -162,10 +162,26 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User
-    .findByIdAndUpdate
-    // After middleware
-    ();
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {new: true}
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
 });
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
@@ -220,4 +236,9 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export {registerUser, loginUser, refreshAccessToken};
+export {
+  registerUser,
+  loginUser,
+  refreshAccessToken,
+  logoutUser
+};
